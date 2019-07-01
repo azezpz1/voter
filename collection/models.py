a0800fdfc0045da6561174a888a83e1b9234f458
Anthony <azezpz1@gmail.com>
2019-06-30 22:25:30 -0500

    Added basic BoardGame model

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class BoardGame(models.Model):
+    yearpublished = models.SmallIntegerField(on_delete=models.CASCADE)
+    minplayers = models.PositiveSmallIntegerField(on_delete=models.CASCADE)
+    maxplayers = models.PositiveSmallIntegerField(on_delete=models.CASCADE)
+    minplaytime = models.PositiveSmallIntegerField(on_delete=models.CASCADE)
+    maxplaytime = models.PositiveSmallIntegerField(on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=4, decimal_places=4)
+    bggurl = models.URLField()
