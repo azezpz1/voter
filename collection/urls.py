ee460fc8abad62fb66724beee1c543d483bdd51a
Anthony Panettiere <azezpz1@gmail.com>
2019-07-02 10:07:55 -0500

    Added add game view

--- collection/urls.py
+++ collection/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from .views import user_home
+from .views import user_home, add_game
 
-urlpatterns = [path("home", user_home, name="user_home")]
+urlpatterns = [
+    path("home", user_home, name="user_home"),
+    path("add_game", add_game, name="add_game"),
+]
 
--- collection/views.py
+++ collection/views.py
@@ -5,3 +5,8 @@ from django.contrib.auth.decorators import login_required
 @login_required()
 def user_home(request):
     return render(request, "collection/user_home.html")
+
+
+@login_required()
+def add_game(request):
+    return render(request, "collection/add_game.html")
