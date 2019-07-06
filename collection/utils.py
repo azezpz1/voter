14a0681146818fe6bf57febc8253523f3b3b5c24
Anthony Panettiere <azezpz1@gmail.com>
2019-07-06 15:16:43 -0500

    Added function to get a boardgame object from bgg

--- collection/utils.py
+++ collection/utils.py
@@ -1,3 +1,6 @@
+from boardgamegeek import BGGClient
+
+
 def get_id_from_bggurl(bggurl: str) -> int:
     """Gets the boardgamegeek id from the provided URL
 
@@ -10,3 +13,17 @@ def get_id_from_bggurl(bggurl: str) -> int:
 
     url_element_list = bggurl.split("/")
     return int(url_element_list[4])
+
+
+def get_boardgame_obj_from_bgg(bggid: str):
+    """Gets a board game object from boardgamegeek.com for the boardgame of the provided ID
+
+    Arguments:
+        bggid {str} -- The boardgamegeek.com ID for the boardgame
+
+    Returns:
+        boardgamegeek.api.game -- A boardgame object with boardgame data
+    """
+
+    client = BGGClient()
+    return client.game(game_id=bggid)
