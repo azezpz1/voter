f3911202de91bca2d1b1d8a224b1c4c1f5156a30
Anthony Panettiere <azezpz1@gmail.com>
2019-07-07 21:27:25 -0500

    Added bggid to the boardgame model

--- collection/models.py
+++ collection/models.py
@@ -12,6 +12,7 @@ class BoardGame(models.Model):
     maxplaytime = models.PositiveSmallIntegerField()
     weight = models.DecimalField(max_digits=4, decimal_places=4)
     bggurl = models.URLField()
+    bggid = models.PositiveSmallIntegerField(unique=True)
 
 
 class SiteUser(models.Model):
